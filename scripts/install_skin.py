bea09f89b75bf93b25663029c7780d30070c5231
lithium <code@hlidskialf.com>
2015-11-22 08:30:31 -0800

    Rename to Juke

--- scripts/install_skin.py
+++ scripts/install_skin.py
@@ -5,12 +5,12 @@ import socket
 
 
 xbmcgui.Dialog().notification('Skin', 'Start skin Update', xbmcgui.NOTIFICATION_INFO, 5000)
-os.system("sudo rm -r /home/pi/skin.CarPC-touch_carbon/");
+os.system("sudo rm -r /home/pi/skin.Juke/");
 xbmcgui.Dialog().notification('Skin', 'Download Update', xbmcgui.NOTIFICATION_INFO, 5000)
-os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon");
+os.system("git clone https://github.com/lithium/skin.Juke");
 xbmcgui.Dialog().notification('Skin', 'Install Update', xbmcgui.NOTIFICATION_INFO, 5000)
-os.system("sudo rm -r /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-os.system("sudo mkdir /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-os.system("sudo rsync -r skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
+os.system("sudo rm -r /home/pi/.kodi/addons/skin.Juke-master/");
+os.system("sudo mkdir /home/pi/.kodi/addons/skin.Juke-master/");
+os.system("sudo rsync -r skin.Juke/ /home/pi/.kodi/addons/skin.Juke-master/");
 xbmcgui.Dialog().notification('Skin', 'Skin Updated', xbmcgui.NOTIFICATION_INFO, 5000)
 xbmc.executebuiltin('ReloadSkin()');
